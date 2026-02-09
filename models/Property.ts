a0a40a5db50b86d4f78dba9abcb6a5d9bb454dbb
Chala Temesgen <ctemesgen85@gmail.com>
2026-02-09 12:15:12 +0300

    property model added

---
+++ models/Property.ts
@@ -0,0 +1,52 @@
+import { Schema, model, models } from "mongoose";
+
+const PropertySchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+
+        description: {
+            type: String,
+            required: true,
+        },
+
+        location: {
+            type: String,
+            required: true,
+        },
+
+        pricePerNight: {
+            type: Number,
+            required: true,
+        },
+
+        images: [String],
+
+        amenities: [String],
+
+        bedrooms: Number,
+        bathrooms: Number,
+        maxGuests: Number,
+
+        host: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        rating: {
+            type: Number,
+            default: 0,
+        },
+
+        reviewCount: {
+            type: Number,
+            default: 0,
+        },
+    },
+    { timestamps: true }
+);
+
+export default models.Property || model("Property", PropertySchema);
